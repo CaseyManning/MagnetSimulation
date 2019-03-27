1a864343faf1d5dc661a246f2f573d2dd0fb6999
Casey Manning <kcgrind@gmail.com>
2019-03-27 13:36:50 -0700

    started work on python script

--- simulation.py
+++ simulation.py
@@ -1,1 +1,21 @@
 import bpy
+
+default_strength = 10
+
+# Class to represent a magnet. Obj variable refers to the Blender representation of the object
+class Magnet:
+    obj = None
+    strength = 2
+    
+    def __init__(self, obj, strength):
+        self.obj = obj
+        self.strength = strength
+
+magnets = []
+
+for obj in bpy.context.scene.objects:
+    if 'Sphere' in obj.name:
+        magnets.append(Magnet(obj, default_strength))
+
+
+print(magnets)
