5d3f3869173ed4c6f07156620a7464c814c0157a
Casey Manning <kcgrind@gmail.com>
2019-10-23 12:02:05 -0700

    continued inntegration

--- addon/__init__.py
+++ addon/__init__.py
@@ -14,7 +14,7 @@ bl_info = {
     "category" : "Generic"
 }
 
-class Test_OT_Operator(bpy.types.Operator):
+class Create_OT_Operator(bpy.types.Operator):
     bl_idname = "view3d.cursor_center"
     bl_label = "simple operator"
     bl_description = "Create Magnets"
@@ -23,18 +23,46 @@ class Test_OT_Operator(bpy.types.Operator):
     #     return context.window_manager.invoke_props_dialog(self)
 
     def execute(self, context):
-
-        bytool = context.scene.by_tool
-
-        if bytool.shape=="Loop":
+        scene = context.scene
+        bytool = scene.by_tool
+        if bytool.Shape=="Loop":
             magnets = MagnetSimulator.loop(bytool.num_magnets, True)
             for i in range(len(magnets)):
-                bpy.ops.mesh.primitive_uv_sphere_add(radius=context.scene.by_tool.num_magnets, enter_editmode=False, location=(magnets[i].position[0], magnets[i].position[1], magnets[i].position[2]))
-            bpy.ops.view3d.snap_cursor_to_center()
-
-        
-        bpy.ops.mesh.primitive_uv_sphere_add(radius=Magnet.radius, enter_editmode=False, location=(1.49944, 0.813217, 3.65082))
+                src_obj = bpy.data.objects["BaseMagnet"]
+                new_obj = src_obj.copy()
+                new_obj.data = src_obj.data.copy()
+                new_obj.animation_data_clear()
+                context.collection.objects.link(new_obj)
+                new_obj.location.x = magnets[i].position[0]*bytool.scale_factor
+                new_obj.location.y = magnets[i].position[1]*bytool.scale_factor
+                new_obj.location.z = magnets[i].position[2]*bytool.scale_factor
+                new_obj.scale.x *= bytool.scale_factor
+                new_obj.scale.y *= bytool.scale_factor
+                new_obj.scale.z *= bytool.scale_factor
+        else:
+            pass
+            
         return {'FINISHED'}
+
+class Clear_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.clear_magnets"
+    bl_label = "Clears Magnets"
+    bl_description = "Clear Magnets"
+
+    # def invoke(self, context, event):
+    #     return context.window_manager.invoke_props_dialog(self)
+
+    def execute(self, context):
+        scene = context.scene
+        bytool = scene.by_tool
+
+        for obj in bpy.data.objects:
+            if object.name.startswith("BaseMagnet") and not object.name == 'BaseMagnet':
+                obj.select_set(True)
+                bpy.ops.object.delete() 
+
+        return {'FINISHED'}
+
 
 def execute_operator(self, context):
     eval('bpy.ops.' + self.primitive + '()')
@@ -44,13 +72,43 @@ class BGProperties(bpy.types.PropertyGroup):
         ("Loop", "Loop", '', 'MESH_CIRCLE', 0),
         ("Saddle", "Saddle", '', '', 1),
         ("Line", "Line", '', '', 2),
-        ("Antitesselated Hypersphere", "Antitesselated Hypersphere", '', '', 3)
+        ("Grid", "Grid", '', '', 3),
+        ("Cylinder", "Cylinder", '', '', 4),
+        ("Antitesselated Hypersphere", "Antitesselated Hypersphere", '', '', 5)
     ]
 
-    shape = bpy.props.EnumProperty(
+    scale_factor: IntProperty(
+        name = "Scale Factor",
+        default = 50,
+        min=1,
+        max=100,
+        description = "Amount to scale the magnet construction in space."
+    )
+
+    Shape = bpy.props.EnumProperty(
         items=mode_options,
         description="Shape of Magnet Construction",
         default="Loop",
+        update=execute_operator
+    )
+
+    show_grid = bpy.props.BoolProperty(
+        name = "Show Grid",
+        description = "Whether or not to show the grid background",
+        default = True
+    )
+
+    auto_clear = bpy.props.BoolProperty(
+        name = "Auto-clear",
+        description = "Whether or not to clear the scene before making a new construction",
+        default = False
+    )
+
+
+    Order = bpy.props.EnumProperty(
+        items=[('Parallel', 'Parallel', '', 0), ('Antiparallel', 'Antiparallel', '', 1)],
+        description="",
+        default="Parallel",
         update=execute_operator
     )
 
@@ -60,8 +118,7 @@ class BGProperties(bpy.types.PropertyGroup):
         default = 0.2,
         min = 0.0,
         max = 1.0
-        )
-    #.......... Float for Generation - Decimation - Angle Limit
+    )
     num_magnets: IntProperty(
         name = "Number of Magnets",
         default = 5,
@@ -80,11 +137,27 @@ class Magnets_PT_Panel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         bytool = context.scene.by_tool
+
         col = layout.column()
+        col.prop(bytool, "scale_factor")
+        col.prop(bytool, "show_grid")
+        col.prop(bytool, "auto_clear")
 
-        layout.prop(bytool, "shape")
+        layout.separator()
 
-        layout.prop(bytool, "num_magnets")
+        box = layout.box()
+        box.label(text="Generation Settings")
+        bcol = box.column()
+
+        bcol.prop(bytool, "Shape")
+
+        layout.separator()
+
+        bcol.prop(bytool, "num_magnets")
+
+        if bytool.Shape == "Grid" or bytool.Shape == "Cylinder":
+            layout.separator()
+            bcol.prop(bytool, "Order")
         
         # col.label(text="Generate objects:")
         # col.prop(context.scene.MagnetGenerator, "primitive")
@@ -92,7 +165,11 @@ class Magnets_PT_Panel(bpy.types.Panel):
         row = layout.row()
         props = row.operator('view3d.cursor_center', text='Create Magnets')
 
-classes = (BGProperties, Magnets_PT_Panel, Test_OT_Operator)
+        layout.separator()
+
+        props2 = layout.operator('view3d.clear_magnets', text='Clear Magnets')
+
+classes = (BGProperties, Magnets_PT_Panel, Create_OT_Operator, Clear_OT_Operator)
 
 # register, unregister = bpy.utils.register_classes_factory(classes)
 
