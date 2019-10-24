a0a6360dde71f5ab1f67b314ef98a0b896573679
Casey Manning <kcgrind@gmail.com>
2019-10-23 21:08:10 -0700

    big refactoring

--- addon/operator.py
+++ addon/operator.py
@@ -1,10 +1,75 @@
 import bpy
+from . StabilitySimulator2 import MagnetSimulator
+from . Magnet import Magnet
 
-class Test_Operator(bpy.types.Operator):
-    bl_idname = "view3d.createMagnets"
+class Create_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.create_magnets"
     bl_label = "simple operator"
     bl_description = "Create Magnets"
 
-    def excecute(self, context):
-        bpy.ops.view3d.snap_cursor_to_center()
+    def execute(self, context):
+        scene = context.scene
+        bytool = scene.by_tool
+        if bytool.auto_clear:
+            bpy.ops.view3d.clear_magnets('INVOKE_DEFAULT')
+        if bytool.Shape=="Loop":
+            magnets = MagnetSimulator.loop(bytool.num_magnets, True)
+        elif bytool.Shape=="Line":
+            magnets = MagnetSimulator.line(bytool.num_magnets, 'x', 'y')
+        else:
+            magnets = []
+        for i in range(len(magnets)):
+            src_obj = bpy.data.objects["BaseMagnet"]
+            new_obj = src_obj.copy()
+            new_obj.data = src_obj.data.copy()
+            new_obj.animation_data_clear()
+            context.collection.objects.link(new_obj)
+            new_obj.location.x = magnets[i].position[0]*bytool.scale_factor
+            new_obj.location.y = magnets[i].position[1]*bytool.scale_factor
+            new_obj.location.z = magnets[i].position[2]*bytool.scale_factor
+            new_obj.scale.x *= bytool.scale_factor
+            new_obj.scale.y *= bytool.scale_factor
+            new_obj.scale.z *= bytool.scale_factor
+        else:
+            pass
+            
+        return {'FINISHED'}
+
+class Clear_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.clear_magnets"
+    bl_label = "Clears Magnets"
+    bl_description = "Clear Magnets"
+
+    # def invoke(self, context, event):
+    #     return context.window_manager.invoke_props_dialog(self)
+
+    def execute(self, context):
+        scene = context.scene
+        bytool = scene.by_tool
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj in bpy.data.objects:
+            if obj.name.startswith("BaseMagnet") and not obj.name == 'BaseMagnet':
+                obj.select_set(True)
+                bpy.ops.object.delete() 
+
+        return {'FINISHED'}
+
+class Clear_Partials_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.clear_partials"
+    bl_label = "Clears Partials"
+    bl_description = "Clears Partials"
+
+    def execute(self, context):
+        scene = context.scene
+        bytool = scene.by_tool
+        return {'FINISHED'}
+
+class Calculate_Partials_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.calc_partials"
+    bl_label = "Calculates Partials"
+    bl_description = "Calculates Partials"
+
+    def execute(self, context):
+        scene = context.scene
+        bytool = scene.by_tool
         return {'FINISHED'}
